deb8c8f5184b3230326db576e04473bb68599546
VitaNYCH <vitanychvoloda@yahoo.com>
2023-04-22 22:45:05 +0300

    Added user info and new folder

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,31 @@
+export const Profile = ({username, tag, location}) => {
+    return (
+      <div className="profile">
+        <div className="description">
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
+            alt="User avatar"
+            className="avatar"
+          />
+          <p className="name">{username}</p>
+          <p className="tag">@{tag}</p>
+          <p className="location">{location}</p>
+        </div>
+
+        <ul className="stats">
+          <li>
+            <span className="label">Followers</span>
+            <span className="quantity">1000</span>
+          </li>
+          <li>
+            <span className="label">Views</span>
+            <span className="quantity">2000</span>
+          </li>
+          <li>
+            <span className="label">Likes</span>
+            <span className="quantity">3000</span>
+          </li>
+        </ul>
+      </div>
+    );
+}
