1cf5943e5b8123116ee7cc3fb2b5922d676cdfd5
Vita Nychvoloda <vitanychvoloda@yahoo.com>
2023-04-29 21:28:31 +0300

    Updated Second section

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,8 @@
 import { Profile } from 'components/Profile/Profile';
+import {Statistics} from 'components/Statistics/Statistics';
 import user from '../user.json';
+import data from '../data.json';
+
 export const App = () => {
   return (
     <div>
@@ -10,6 +13,7 @@ export const App = () => {
   avatar={user.avatar}
   stats={user.stats}
 />
+<Statistics title="Upload stats" stats={data} />
     </div>
   );
 };
