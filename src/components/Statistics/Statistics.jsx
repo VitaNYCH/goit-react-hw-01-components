6c6dc66257b0da541d3cbddaafeb5226a0b1371d
Vita Nychvoloda <vitanychvoloda@yahoo.com>
2023-05-16 16:07:45 +0300

    Created section Statistics

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,26 +1,21 @@
+import PropTypes from 'prop-types';
 import css from './Statistics.module.css';
 
-export const Statistics = ({title, stats})=>{
+export const Statistics = ({title , stats})=>{
+  console.log(stats);
     return (<section className={css.statistics}>
-    <h2 className={css.title}>{title}</h2>
-  
-    <ul className="stat-list">
-      <li className={css.item}>
-        <span className={css.label}>.docx</span>
-        <span className={css.percentage}>4%</span>
-      </li>
-      <li className={css.item}>
-        <span className={css.label}>.mp3</span>
-        <span className={css.percentage}>14%</span>
-      </li>
-      <li className={css.item}>
-        <span className={css.label}>.pdf</span>
-        <span className={css.percentage}>41%</span>
-      </li>
-      <li className={css.item}>
-        <span className={css.label}>.mp4</span>
-        <span className={css.percentage}>12%</span>
-      </li>
-    </ul>
+    {title && <h2 className={css.title}>{title}</h2>}
+    
+    <ul className = {css.statlist}>
+{stats.map(stat=> <li className={css.item} key = {stat.id}><span className={css.label}> {stat.label}</span>
+  <span className={css.percentage}>{stat.percentage}%</span></li>)}
+</ul> 
   </section>)
 }
+
+Statistics.propTypes={
+  title: PropTypes.string,
+  stats: PropTypes.array,
+}
+export default Statistics;
+
