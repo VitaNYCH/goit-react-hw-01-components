7ccc8043229f8781937b7746170ca72e2f72efc6
Vita Nychvoloda <vitanychvoloda@yahoo.com>
2023-05-16 17:58:24 +0300

    Created list of Friends

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,14 @@
 import { Profile } from 'components/Profile/Profile';
-import Statistics from 'components/Statistics/Statistics';
+import{Statistics} from 'components/Statistics/Statistics';
+import {FriendList} from 'components/FriendList/FriendList';
 import user from '../user.json';
 import data from '../data.json';
+import friends from '../friends.json';
 
 export const App = () => {
   return (
     <div>
+ 
  <Profile
   username={user.username}
   tag={user.tag}
@@ -13,9 +16,12 @@ export const App = () => {
   avatar={user.avatar}
   stats = {user.stats}
 />
+
 <Statistics 
 title="Upload stats" 
 stats={data} />
+
+<FriendList friends={friends} />
     </div>
   );
 };
